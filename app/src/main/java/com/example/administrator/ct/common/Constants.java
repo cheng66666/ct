d68f62b57bda1330fd8b5ceaaa762d083f6b1e99
cheng66666 <1292455267@qq.com>
2019-11-29 15:43:17 +0800

    增加主页模块

--- app/src/main/java/com/example/administrator/ct/common/Constants.java
+++ app/src/main/java/com/example/administrator/ct/common/Constants.java
@@ -2,6 +2,6 @@ package com.example.administrator.ct.common;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://120.24.103.163:8080/MobileShop/";
+    public static final String BASE_URL = "http://10.216.13.209:8080/MobileShop/";
 
 }
